11c04374bd9963d3fa4af7a31cb88a0098af9980
Michael Le-Reiver <m.lereiver@gmail.com>
2018-07-23 09:13:51 -0700

    completed 2.Array Double

--- recursion.js
+++ recursion.js
@@ -1,29 +1,44 @@
 ////week 1 monday jul 23 recursion
 
-function sumOf(list){
+// function sumOf(list){
 
-    if(list.length === 1){
-        return list[0];
-    }
-    return list[0] + sumOf(list.slice(1));
-}
+//     if(list.length === 1){
+//         return list[0];
+//     }
+//     return list[0] + sumOf(list.slice(1));
+// }
 
-let lst = [2,4,6,8];
-console.log("problem 1")
-console.log(sumOf(lst));
+// let lst = [2,4,6,8];
+// console.log("problem 1")
+// console.log(sumOf(lst));
 
 /////problem 1 recursion drills
-console.log("problem2");
+
+console.log("1. Counting Sheep");
+
 function countingSheep(sheep){
     if(sheep === 0){
-        return "no more sheep"
+        return "no more sheep";
     }
-    console.log(sheep);
-    let currentsheep = countingSheep(sheep - 1) 
-    
-    return currentsheep
+    console.log(sheep, ' - Another sheep jumped over the fence');
+    let currentsheep = countingSheep(sheep - 1);
+    return currentsheep;
 }
-let sheeple = 10;
-let newsheepcount = countingSheep(sheeple);
 
-console.log(`${newsheepcount} another sheep jumped over the fence `);
+console.log(countingSheep(3));
+
+console.log('+++++++++++++++++++++++++++++++++++++++++');
+console.log('2. Array Double');
+
+function arrayDouble(arr) {
+// base case 
+  if (!arr.length) {
+    return;
+  }
+  // recursion
+  // for each element double the value
+  return arr.map(el => el*2);
+}
+
+console.log(arrayDouble([3,4,5,6]));
+console.log(arrayDouble([5,30,522,-0.26]));
